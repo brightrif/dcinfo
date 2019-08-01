e62784fa2cca0e284b4597dc046fffefc3dc12e9
Bright Antony <brightantony.g@gmail.com>
2019-08-01 18:35:42 +0300

    module created

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class servertype(models.Model):
+    serverType = models.CharField(max_length=30)
+
+class center(models.Model):
+    center = models.CharField(max_length=30)
+
+class bladeinfo(models.Model):
+    bladeId = models.CharField(max_length=30,primary_key=True)
+    bladeNo   = models.IntegerField()
+    bladeSerial = models.CharField(max_length=30)
+
+class bladechassis(models.Model):
+    bladeChassisId = models.CharField(max_length=30,primary_key=True)
+    bladeChassisNo = models.IntegerField()
+    bladeChassisSerial = models.CharField(max_length=30)
+
+class serverinfo(models.Model):
+    ipaddress = models.GenericIPAddressField()
+    os = models.CharField(max_length=30)
+
+class esxi(models.Model):
+    #have to change the Musician becasue copied form other site
+    inventoryID = models.CharField(max_length=30)
+    esxiid = models.CharField(max_length=100, primary_key=True)
+    ipaddress = models.GenericIPAddressField()
+    bladechassisNo = models.IntegerField()
+    bladeNo = models.IntegerField()
